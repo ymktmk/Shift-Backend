21a3673d02ecee87d1f57d96aadccde1de76bf1f
ymktmk <ymktmk.co@gmail.com>
2022-03-02 00:10:20 +0900

    [add] firebase_auth

---
+++ README.md
@@ -0,0 +1,5 @@
+# 参考
+
+Go言語でEchoを用いて認証付きWebアプリの作成
+https://qiita.com/x-color/items/24ff2491751f55e866cf
+
--- domain/user.go
+++ domain/user.go
@@ -3,6 +3,7 @@ package domain
 type User struct {
     ID   int    `json:"id"`
     Name string `json:"name" required:"true"`
+    Email string `json:"email"`
 }
 
 type Users []User
